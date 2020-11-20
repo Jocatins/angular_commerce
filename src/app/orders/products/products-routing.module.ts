60d479a694f0a2de42cb225f94aa715915edfbcc
Nicholas Agwah <nagwah@teqbridgeltd.com>
2020-11-20 15:28:10 +0100

    routing updates

--- src/app/orders/products/products-routing.module.ts
+++ src/app/orders/products/products-routing.module.ts
@@ -5,16 +5,20 @@ import { DeleteProductComponent } from './delete-product/delete-product.componen
 
 import { ProductsComponent } from './products.component';
 import { UpdateProductComponent } from './update-product/update-product.component';
+import { ViewAllProductByCategoryComponent } from './view-all-product-by-category/view-all-product-by-category.component';
+import { ViewAllProductByDateComponent } from './view-all-product-by-date/view-all-product-by-date.component';
 import { ViewAllProductComponent } from './view-all-product/view-all-product.component';
 import { ViewProductComponent } from './view-product/view-product.component';
 
 const routes: Routes = [
   { path: '', component: ProductsComponent },
   { path: 'create', component: CreateProductComponent },
-  { path: 'delete', component: DeleteProductComponent },
-  { path: 'update', component: UpdateProductComponent },
+  { path: 'delete/:id', component: DeleteProductComponent },
+  { path: 'update/:id', component: UpdateProductComponent },
   { path: 'view-all', component: ViewAllProductComponent },
-  { path: 'view-product', component: ViewProductComponent },
+  { path: 'view-product/:id', component: ViewProductComponent },
+  { path: 'search', component: ViewAllProductByCategoryComponent },
+  { path: 'search-date', component: ViewAllProductByDateComponent },
 ];
 
 @NgModule({
--- src/app/orders/products/view-all-product-by-category/view-all-product-by-category.component.ts
+++ src/app/orders/products/view-all-product-by-category/view-all-product-by-category.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-all-product-by-category',
   templateUrl: './view-all-product-by-category.component.html',
-  styleUrls: ['./view-all-product-by-category.component.css']
+  styleUrls: ['./view-all-product-by-category.component.css'],
 })
 export class ViewAllProductByCategoryComponent implements OnInit {
-
-  constructor() { }
+  searchCategory = '';
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((data) => {
+      this.searchCategory = data.category;
+    });
   }
-
 }
